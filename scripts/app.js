9769ad7c7a8932ebd8cb974a6161053725065746
Zahirul Islam Tanvir <zahirulopel@gmail.com>
2022-08-03 12:04:54 +0600

    Change the backgound color of boxs when mouse hover over it
    First we select all the boxs we wanted to change color with js.
    By add event for each box when a mouse hover on a box we change
    the backgound color of with with inner js.

--- scripts/app.js
+++ scripts/app.js
@@ -8,3 +8,11 @@ for (let row = 0; row < 16; row++) {
         containerEl.appendChild(createDiv);
     }
 }
+// Pixel change when hover over a box
+// Select all square box in the page
+let allBoxEl = document.querySelectorAll(".box");
+console.log(allBoxEl);
+function changeBoxColor(e) {
+    e.target.style.backgroundColor = "purple";
+}
+allBoxEl.forEach((box) => box.addEventListener("mouseover", changeBoxColor));
