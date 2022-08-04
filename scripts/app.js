904979864ece148e07d2263859a260654e1f7969
Zahirul Islam Tanvir <zahirulopel@gmail.com>
2022-08-04 11:50:39 +0600

    Divide the sketch pad screen into custom size
    By input a number in the field user can create that number of little
    screen for the sketch pad.
    
    Rather than fixed size screen by adjusting custom screen user has the
    ability to draw large or small object.
    
    When user fill the box with number then click genarate this button
    will create the number of box in the both row and column.As our
    total screen width and height are fixed we have to change the little
    box so they can fit in the large screen.

--- scripts/app.js
+++ scripts/app.js
@@ -1,18 +1,46 @@
 let containerEl = document.querySelector(".container");
 console.log(containerEl);
-for (let row = 0; row < 16; row++) {
-    for (let col = 0; col < 16; col++) {
-        let createDiv = document.createElement("div");
-        createDiv.classList.add("box");
-        createDiv.textContent = `${col}`;
-        containerEl.appendChild(createDiv);
+
+// Get user input
+let userInputSquare = document.querySelector("input");
+document.querySelector("button").addEventListener("click", function (e) {
+    console.log(userInputSquare.value);
+    console.log(typeof userInputSquare.value);
+    // Convert user input string to number
+    let row_sqr = Number(userInputSquare.value);
+    let col_sqr = row_sqr;
+    console.log(typeof col_sqr);
+
+    // Create div with custom row and column
+    // If user input 6 , row_sqr and col_sql will be 6
+    for (let row = 0; row < row_sqr; row++) {
+        for (let col = 0; col < col_sqr; col++) {
+            let createDiv = document.createElement("div");
+            createDiv.classList.add("box");
+            createDiv.textContent = `${col}`;
+            containerEl.appendChild(createDiv);
+        }
     }
-}
-// Pixel change when hover over a box
-// Select all square box in the page
-let allBoxEl = document.querySelectorAll(".box");
-console.log(allBoxEl);
-function changeBoxColor(e) {
-    e.target.style.backgroundColor = "purple";
-}
-allBoxEl.forEach((box) => box.addEventListener("mouseover", changeBoxColor));
+
+    // Select all the little square box element
+    let allBoxEl = document.querySelectorAll(".box");
+    console.log("allbox:", allBoxEl);
+    // Set the width and height of the little square box
+    // So they can be adjust side by side,top and bottom
+    let set_width = 100 / row_sqr;
+    let set_height = set_width;
+    allBoxEl.forEach((small_box) => (small_box.style.width = `${set_width}%`));
+    allBoxEl.forEach(
+        (small_box) => (small_box.style.height = `${set_height}%`)
+    );
+
+    // By hover the mouse the background color of a little sqr
+    // box will change
+    function changeBoxColor(e) {
+        e.target.style.backgroundColor = "purple";
+    }
+    allBoxEl.forEach((box) =>
+        box.addEventListener("mouseover", changeBoxColor)
+    );
+});
+
