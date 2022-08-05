8fceda0dd9ebfb85c74eeaa98a3064c0cfd88e01
Zahirul Islam Tanvir <zahirulopel@gmail.com>
2022-08-05 11:26:10 +0600

    Change the sketch pad screen color to random color
    Previous the sketch pad screen color was a fixed color(black).Now the color
    will be different for each single screen of sketch pad
    
    Single color is booring.User may lose interest to play.So added random color
    for different feeling.
    
    Added a button name random color,by press it will active a class.When we
    change sketch pad screen color if we see the class is there we will change
    the screen pad color to random.

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,14 @@
 let containerEl = document.querySelector(".container");
 
+// Feature custom color
+let customColorBtn = document.querySelector(".random-color");
+customColorBtn.addEventListener("click", function (e) {
+    customColorBtn.classList.toggle("true");
+});
+
+// Rainbow
+// rgb 0 black - 1 white
+
 // Get user input
 let userInputSquare = document.querySelector("input");
 document.querySelector("button").addEventListener("click", function (e) {
@@ -33,10 +42,24 @@ document.querySelector("button").addEventListener("click", function (e) {
         (small_box) => (small_box.style.height = `${set_height}%`)
     );
 
+    // Random number genarator
+    let randomNumber = function () {
+        //  random number between 0 - 255
+        return Math.floor(Math.random() * 255);
+    };
+
     // By hover the mouse the background color of a little sqr
     // box will change
     function changeBoxColor(e) {
-        e.target.style.backgroundColor = "purple";
+        // Random color
+        if (customColorBtn.classList.contains("true")) {
+            let red = randomNumber();
+            let blue = randomNumber();
+            let green = randomNumber();
+            e.target.style.backgroundColor = `rgb(${red},${blue},${green})`;
+        } else {
+            e.target.style.backgroundColor = "black";
+        }
     }
     allBoxEl.forEach((box) =>
         box.addEventListener("mouseover", changeBoxColor)
